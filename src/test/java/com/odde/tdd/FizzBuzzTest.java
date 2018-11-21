12fd4ceefdcbabc0b668e75ba3a43553ffa47efa
Xing, Wang <xwang16@paypal.com>
2018-11-21 10:26:52 +0800

    separate diff test cases into diff methods

--- src/test/java/com/odde/tdd/FizzBuzzTest.java
+++ src/test/java/com/odde/tdd/FizzBuzzTest.java
@@ -6,12 +6,26 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void testFizzBuzz(){
-        assertEquals("1", FizzBuzz.report(1));
+    public void testMultiplesOfThree() {
         assertEquals("Fizz", FizzBuzz.report(3));
+        assertEquals("Fizz", FizzBuzz.report(6));
+    }
+
+    @Test
+    public void testMultiplesOfFive() {
         assertEquals("Buzz", FizzBuzz.report(5));
+        assertEquals("Buzz", FizzBuzz.report(10));
+    }
+
+    @Test
+    public void testNormalNumbers() {
+        assertEquals("1", FizzBuzz.report(1));
+        assertEquals("31", FizzBuzz.report(31));
+    }
+
+    @Test
+    public void testFizzBuzz(){
         assertEquals("FizzBuzz", FizzBuzz.report(15));
         assertEquals("FizzBuzz", FizzBuzz.report(30));
-        assertEquals("31", FizzBuzz.report(31));
     }
 }
